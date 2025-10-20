17106cccbc7a1fca246ed47070690a7bcc4afd1b
Alejandro <3512126@gmail.com>
2025-10-20 17:36:32 +0200

    Added the excercise files

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("PingPong")
+        elif i % 3 == 0:
+            print("ping")
+        elif i % 5 == 0:
+            print("pong")
+        else:
+            print(i)
+
+if __name__ == "__main__":
+    main()
